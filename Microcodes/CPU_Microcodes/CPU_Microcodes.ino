9806bffce92b055942ed9657e3ecde7217ace810
blintux <blintux@gmail.com>
2018-12-10 20:28:57 +0100

    64K version updates BETA!

--- Microcodes/CPU_Microcodes/CPU_Microcodes.ino
+++ Microcodes/CPU_Microcodes/CPU_Microcodes.ino
@@ -36,57 +36,67 @@
 
 // ROM 4
 #define ST  0b00000000000000000000000010000000    // Stack select
-#define NA4 0b00000000000000000000000001000000    // ---
+#define BO  0b00000000000000000000000001000000    // B register output
 #define NA5 0b00000000000000000000000000100000    // ---
 #define NA6 0b00000000000000000000000000010000    // ---
 #define NA7 0b00000000000000000000000000001000    // ---
-#define RCS 0b00000000000000000000000000000100    // RTC: Chis Select
+#define SND 0b00000000000000000000000000000100    // Send data to Sound chip
 #define ROE 0b00000000000000000000000000000010    // RTC: Output Enable
 #define RRW 0b00000000000000000000000000000001    // RTC: Read/Write
+
 
 // Fetch (T1, T2)
 #define FETCH MI|CO, RO|II|CE
 
 // Null values ( T11 -> T16 )
-#define NULLS 0,0,0,0,0,0
+#define NULLS ICR,0,0,0,0,0
+
 
 const PROGMEM uint32_t data[] = {
 /*
-T1,T2   T3          T4           T5             T6           T7             T8        T9          T10  T11+        CMD    BIN     HEX    ADDRESSING
+T1,T2   T3          T4           T5             T6           T7          T8         T9          T10     T11-T16     CMD    BIN     HEX    ADDRESSING
 */
-FETCH,  ICR,        0,           0,             0,           0,             0,        0,          0,   NULLS,   // NOP    00000   0x00   Implied
-FETCH,  CO|MI,      RO|MI|CE,    RO|AI|SF,      ICR,         0,             0,        0,          0,   NULLS,   // LDA    00010   0x10   Absolute
-FETCH,  CO|MI,      RO|MI|CE,    RO|BI,         AI|SF|EO,    ICR,           0,        0,          0,   NULLS,   // ADC    00100   0x20   Absolute
-FETCH,  CO|MI,      RO|MI|CE,    RO|BI,         AI|SU|SF|EO, ICR,           0,        0,          0,   NULLS,   // SUB    00110   0x30   Absolute
-FETCH,  CO|MI,      RO|MI|CE,    AO|RI,         ICR,         0,             0,        0,          0,   NULLS,   // STA    01000   0x40   Absolute
-FETCH,  CO|MI,      RO|AI|CE,    SF,            ICR,         0,             0,        0,          0,   NULLS,   // LDI    01010   0x05   Immediate
-FETCH,  CO|MI,      RO|J,        ICR,           0,           0,             0,        0,          0,   NULLS,   // JMP    01100   0x60   Absolute
-FETCH,  CO|MI,      RO|BI|CE,    AI|SU|SF|EO,   ICR,         0,             0,        0,          0,   NULLS,   // SUB    01110   0x70   Immediate
-FETCH,  IO|RRW|RCS, CO|MI,       RO|MI|CE,      RCS|ROE|RI,  CO|MI,         RO|MI|CE, RCS|ROE|RI, ICR, NULLS,   // RTR    10000   0x08   Absolute
-FETCH,  IO|RRW|RCS, CO|MI,       RO|CE|RCS|RRW, CO|MI,       RO|CE|RCS|RRW, ICR,      0,          0,   NULLS,   // RTW    10010   0x09   Immediate
-FETCH,  ST|MI,      ST|CO|RI,    CO|MI,         RO|J,        ICR,           0,        0,          0,   NULLS,   // JSR    10100   0xA0   Absolute
-FETCH,  ST|MI,      ST|RO|J,     CE,            ICR,         0,             0,        0,          0,   NULLS,   // RTS    10110   0xB0   Implied
-FETCH,  IP|AI|SF,   IP|ICR,      0,             0,           0,             0,        0,          0,   NULLS,   // IPR    11000   0xC0   Immediate
-FETCH,  CO|MI,      RO|MI|CE,    IP|RI,         IP|ICR,      0,             0,        0,          0,   NULLS,   // IPR    11010   0xD0   Absolute
-FETCH,  CO|MI,      CE|RO|O2|O1, RO|O3|O1,      RO|O2|O3|O1, RO,            ICR,      0,          0,   NULLS,   // OUT.d  11100   0xE0   Immediate
-FETCH,  HLT,        ICR,         0,             0,           0,             0,        0,          0,   NULLS,   // HLT    11110   0xF0   Implied
-FETCH,  CO|MI,      RO|JZ,       CE,            ICR,         0,             0,        0,          0,   NULLS,   // BEQ    00001   0x08   Absolute
-FETCH,  CO|MI,      RO|MI|CE,    RO|BI,         SU|SF|EO,    ICR,           0,        0,          0,   NULLS,   // CMP    00011   0x18   Absolute
-FETCH,  CO|MI,      RO|JC,       CE,            ICR,         0,             0,        0,          0,   NULLS,   // BCS    00101   0x28   Absolute
-FETCH,  CO|MI,      RO|JN,       CE,            ICR,         0,             0,        0,          0,   NULLS,   // BMI    00111   0x38   Absolute
-FETCH,  AO|BI,      AI|SF|EO,    ICR,           0,           0,             0,        0,          0,   NULLS,   // ASL    01001   0x48   Implied
-FETCH,  AO|BI,      AI|SU|SF|EO, ICR,           0,           0,             0,        0,          0,   NULLS,   // LSR    01011   0x58   Implied
-FETCH,  CO|MI,      RO|BI|CE,    AI|SF|EO,      ICR,         0,             0,        0,          0,   NULLS,   // ADC    01101   0x68   Immediate
-FETCH,  CO|MI,      RO|BI|CE,    SU|SF|EO,      ICR,         0,             0,        0,          0,   NULLS,   // CMP    01111   0x78   Immediate
-FETCH,  CO|MI,      RO|MI|CE,    RO|O2|O3,      RO,          ICR,           0,        0,          0,   NULLS,   // LCD    10001   0x88   Absolute
-FETCH,  CO|MI,      RO|O2|O3|CE, ICR,           0,           0,             0,        0,          0,   NULLS,   // LCD    10011   0x98   Immediate
-FETCH,  AO|O2|O3,   AO,          ICR,           0,           0,             0,        0,          0,   NULLS,   // LCD    10101   0xA8   Implied
-FETCH,  CO|MI,      RO|MI|CE,    RO|O1,         RO,          ICR,           0,        0,          0,   NULLS,   // OUT.c  10111   0xB8   Absolute
-FETCH,  CO|MI,      CE|RO|O1,    RO,            ICR,         0,             0,        0,          0,   NULLS,   // OUT.c  11001   0xC8   Immediate
-FETCH,  AO|MI,      RO|O1,       RO,            ICR,         0,             0,        0,          0,   NULLS,   // OUT.c  11011   0xD8   Implied
-FETCH,  CO|MI,      RO|MI|CE,    RO|O2|O1,      RO|O3|O1,    RO|O2|O3|O1,   RO,       ICR,        0,   NULLS,   // OUT.d  11101   0xE8   Absolute
-FETCH,  AO|O2|O1,   AO|O3|O1,    AO|O2|O3|O1,   AO,          ICR,           0,        0,          0,   NULLS,   // OUT.d  11111   0xF8   Implied
+FETCH,  ICR,        0,           0,             0,           0,          0,         0,          0,      NULLS,   // NOP    00000   0x00   Implied
+FETCH,  CO|MI,      RO|MI|CE,    RO|AI|SF,      ICR,         0,          0,         0,          0,      NULLS,   // LDA    00010   0x10   Absolute
+FETCH,  CO|MI,      RO|MI|CE,    RO|BI,         AI|SF|EO,    ICR,        0,         0,          0,      NULLS,   // ADC    00100   0x20   Absolute
+FETCH,  CO|MI,      RO|MI|CE,    RO|BI,         AI|SU|SF|EO, ICR,        0,         0,          0,      NULLS,   // SUB    00110   0x30   Absolute
+FETCH,  CO|MI,      RO|MI|CE,    AO|RI,         ICR,         0,          0,         0,          0,      NULLS,   // STA    01000   0x40   Absolute
+FETCH,  CO|MI,      RO|AI|CE,    SF,            ICR,         0,          0,         0,          0,      NULLS,   // LDI    01010   0x05   Immediate
+FETCH,  CO|MI,      RO|J,        ICR,           0,           0,          0,         0,          0,      NULLS,   // JMP    01100   0x60   Absolute
+FETCH,  CO|MI,      RO|BI|CE,    AI|SU|SF|EO,   ICR,         0,          0,         0,          0,      NULLS,   // SUB    01110   0x70   Immediate
+FETCH,  CO|MI,      CE|RO|RRW,   CO|MI,         RO|MI|CE,    ROE|RI,     CO|MI,     RO|MI|CE,   ROE|RI, NULLS,   // RTR    10000   0x80   Absolute
+FETCH,  CO|MI,      CE|RO|RRW,   CO|MI,         RO|CE|RRW,   CO|MI,      RO|CE|RRW, ICR,        0,      NULLS,   // RTW    10010   0x90   Immediate
+FETCH,  ST|MI,      ST|CO|RI,    CO|MI,         RO|J,        ICR,        0,         0,          0,      NULLS,   // JSR    10100   0xA0   Absolute
+FETCH,  ST|MI,      ST|RO|J,     CE,            ICR,         0,          0,         0,          0,      NULLS,   // RTS    10110   0xB0   Implied
+FETCH,  IP|AI|SF,   IP|ICR,      0,             0,           0,          0,         0,          0,      NULLS,   // IPR    11000   0xC0   Immediate
+FETCH,  CO|MI,      RO|MI|CE,    IP|RI,         IP|ICR,      0,          0,         0,          0,      NULLS,   // IPR    11010   0xD0   Absolute
+FETCH,  CO|MI,      CE|RO|O3|O1, RO|O1|O2,      RO|O1,       RO,         ICR,       0,          0,      NULLS,   // OUT.d  11100   0xE0   Immediate
+FETCH,  HLT,        ICR,         0,             0,           0,          0,         0,          0,      NULLS,   // HLT    11110   0xF0   Implied
+FETCH,  CO|MI,      RO|JZ,       CE,            ICR,         0,          0,         0,          0,      NULLS,   // BEQ    00001   0x08   Absolute
+FETCH,  CO|MI,      RO|MI|CE,    RO|BI,         SU|SF|EO,    ICR,        0,         0,          0,      NULLS,   // CMP    00011   0x18   Absolute
+FETCH,  CO|MI,      RO|JC,       CE,            ICR,         0,          0,         0,          0,      NULLS,   // BCS    00101   0x28   Absolute
+//FETCH,CO|MI,      RO|JN,       CE,            ICR,         0,          0,         0,          0,      NULLS,   // BMI    00111   0x38   Absolute
+FETCH,  CO|MI,      CE|RO|SND,   RO|SND,        RO|SND,      ICR,        0,         0,          0,      NULLS,   // SND    00111   0x38   Absolute
+FETCH,  AO|BI,      AI|SF|EO,    ICR,           0,           0,          0,         0,          0,      NULLS,   // ASL    01001   0x48   Implied
+FETCH,  CO|MI,      RO|BI|CE,    AO|MI,         BO|RI,       ICR,        0,         0,          0,      NULLS,   // STA.a  01011   0x58
+//FETCH,AO|BI,      AI|SU|SF|EO, ICR,           0,           0,          0,         0,          0,      NULLS,   // LSR    01011   0x58   Implied
+FETCH,  CO|MI,      RO|BI|CE,    AI|SF|EO,      ICR,         0,          0,         0,          0,      NULLS,   // ADC    01101   0x68   Immediate
+FETCH,  CO|MI,      RO|BI|CE,    SU|SF|EO,      ICR,         0,          0,         0,          0,      NULLS,   // CMP    01111   0x78   Immediate
+FETCH,  AO|MI,      RO|AI|SF,    ICR,           0,           0,          0,         0,          0,      NULLS,   // LDA.a  10001   0x88   Absolute
+//FETCH,CO|MI,      RO|MI|CE,    RO|O2|O3,      RO,          ICR,        0,         0,          0,      NULLS,   // LCD    10001   0x88   Absolute
+FETCH,  CO|MI,      RO|O2|O3,    CE,            0,           0,          0,         0,          ICR,    NULLS,   // LCD    10011   0x98   Immediate
+FETCH,  AO|O2|O3,   AO,          0,             0,           0,          0,         0,          ICR,    NULLS,   // LCD    10101   0xA8   Implied
+FETCH,  CO|MI,      RO|MI|CE,      RO|O1|O2|O3, RO,          ICR,        0,         0,          0,      NULLS,   // OUT.c  10111   0xB8   Absolute
+FETCH,  CO|MI,      CE|RO|O1|O2|O3,RO,          ICR,         0,          0,         0,          0,      NULLS,   // OUT.c  11001   0xC8   Immediate
+FETCH,  AO|MI,      RO|O1|O2|O3,   RO,          ICR,         0,          0,         0,          0,      NULLS,   // OUT.c  11011   0xD8   Implied
+FETCH,  CO|MI,      RO|MI|CE,    RO|O3|O1,      RO|O1|O2,    RO|O1,      RO,        ICR,        0,      NULLS,   // OUT.d  11101   0xE8   Absolute
+FETCH,  AO|O3|O1,   AO|O1|O2,    AO|O1,         AO,          ICR,        0,         0,          0,      NULLS,   // OUT.d  11111   0xF8   Implied
 };
+
+//Absolute  : Megadott memoria cim
+//Immediate : Kozvetlen ertek
+//Implied   : A Reg
+
 
 
 /*
@@ -139,6 +149,25 @@ void writeEEPROM(int address, byte data) {
   delay(10);
 }
 
+/*
+ * Read a byte from the EEPROM at the specified address.
+ */
+byte readEEPROM(int address) {
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
+    pinMode(pin, INPUT);
+  }
+  setAddress(address, /*outputEnable*/ true);
+
+  byte data = 0;
+  for (int pin = EEPROM_D7; pin >= EEPROM_D0; pin -= 1) {
+    data = (data << 1) + digitalRead(pin);
+  }
+  return data;
+}
+
+/*
+ * Init the EEPROM Writer
+ */
 void setup() {
   pinMode(SHIFT_DATA, OUTPUT);
   pinMode(SHIFT_CLK, OUTPUT);
@@ -147,12 +176,15 @@ void setup() {
   pinMode(WRITE_EN, OUTPUT);
   Serial.begin(57600);
 
-  Serial.println("Write EEPROMs: 1, 2, 3, 4 (n = EEPROM number)");
-  Serial.println("Write shifted data: a \n");
-  Serial.println("Erase Command: e\n");
+  Serial.println("Write specified EEPROM: 1, 2, 3, 4 (n = EEPROM number)");
+  Serial.println("Write shifted data: a");
+  Serial.println("Erase EEPROM: e");
+  Serial.println("Read EEPROM: r \n");
 }
 
-
+/*
+ * Program loop
+ */
 void loop() {
   if (Serial.available() > 0) {
     command = Serial.read();
@@ -162,40 +194,64 @@ void loop() {
       //Erase EEPROM
       case 'e':
         Serial.print("Erasing EEPROM .");
-        for (int address = 0; address <= 2047; address += 1) {
+        
+        for (int address = 0; address <= 512; address += 1) {
           //writeEEPROM(address, 0xff);
-          if (address % 128 == 0) Serial.print(".");
+          if (address % 64 == 0) Serial.print(".");
         }
-        Serial.print(" Done!\n");
+
+        Serial.print(" Done!\n\n");
+      break;
+
+      //Read EEPROM
+      case 'r':
+        Serial.print("Reading EEPROM ... \n\n");
+      
+        for (int base = 0; base <= 512; base += 16) {
+          byte data[16];
+          for (int offset = 0; offset <= 15; offset += 1) {
+            data[offset] = readEEPROM(base + offset);
+          }
+      
+          char buf[80];
+          sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
+                  base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+                  data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
+      
+          Serial.println(buf);
+        }
       break;
 
       //Write EEPROM
+      case '5':
       case '4':
       case '3':
       case '2':
       case '1':
-        Serial.print("Writing (EEPROM ");
-        Serial.print( char(command) );
-        Serial.print(") .");
-      
+        Serial.print("Writeing EEPROM .");
+        
+        writeEEPROM( 0x200, 0x2 );
+        writeEEPROM( 0x201, 0x4 );
+        writeEEPROM( 0x202, 0x6 );
+        writeEEPROM( 0x203, 0x8 );
+
         for (int address = 0; address < sizeof(data)/sizeof( pgm_read_dword(data + 0) ); address += 1) {
 /*
-          if ( command == '1' )  writeEEPROM(address + 1536, pgm_read_dword(data + address) >> 0 );
-          if ( command == '2' )  writeEEPROM(address + 1024, pgm_read_dword(data + address) >> 8 );
-          if ( command == '3' )  writeEEPROM(address +  512, pgm_read_dword(data + address) >> 16 );
-          if ( command == '4' )  writeEEPROM(address,        pgm_read_dword(data + address) >> 24 );
+          if ( command == '1' )  writeEEPROM(address + 2048, pgm_read_dword(data + address) >> 0 );
+          if ( command == '2' )  writeEEPROM(address + 1536, pgm_read_dword(data + address) >> 8 );
+          if ( command == '3' )  writeEEPROM(address + 1024, pgm_read_dword(data + address) >> 16 );
+          if ( command == '4' )  writeEEPROM(address +  512, pgm_read_dword(data + address) >> 24 );
+          if ( command == '5' )  writeEEPROM(address,        pgm_read_dword(data + address) >> 32 );
 */
           if (address % 64 == 0) Serial.print(".");
         }
-        
+
         Serial.print(" Done!\n\n");
-        
       break;
 
       // Write all EEPROM
       case 'a':
-      
-        Serial.print("Writing EEPROM .");
+        Serial.print("Writeing EEPROM .");
       
         for (int address = 0; address < sizeof(data)/sizeof( pgm_read_dword(data + 0) ); address += 1) {
           writeEEPROM(address + 1536, pgm_read_dword(data + address) >> 0 );
@@ -205,9 +261,10 @@ void loop() {
 
           if (address % 64 == 0) Serial.print(".");
         }
-        
+
         Serial.print(" Done!\n\n");
       break;
+
     }
   }
 }
--- Microcodes/LCD_Controller/LCD_Controller.ino
+++ Microcodes/LCD_Controller/LCD_Controller.ino
@@ -1,7 +1,3 @@
-/**
- * This sketch is specifically for programming the EEPROM used in the 8-bit
- * decimal display decoder described in https://youtu.be/dLh1n2dErzE
- */
 #define SHIFT_DATA 2
 #define SHIFT_CLK 3
 #define SHIFT_LATCH 4
@@ -91,7 +87,7 @@ void setup() {
 
 
   // Erase entire EEPROM
-/*
+  /*
   Serial.print("Erasing EEPROM");
   for (int address = 0; address <= 2047; address += 1) {
     writeEEPROM(address, 0xff);
@@ -101,66 +97,57 @@ void setup() {
     }
   }
   Serial.println(" done");
-*/
+  */
 
   // Bit patterns for the digits 0..9
-  //byte digits[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b };
-  //byte digits[] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
   byte digits[] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 };
 
 
 /*
+          RS
+  RRW ROE O1
+   |   |   | 
+   0   0   0    Init code
+   1   1   0    Commands
+   0   1   0    Defined commands
+   
+   0   0   1    Characters
+   0   1   1    Hundreds place
+   1   0   1    Tens place
+   1   1   1    Ones place
 
-RRW ROE RCS
- |   |   | 
- 1   1   0    Parancsok
- 0   1   0    Definiált parancsok
- 0   0   1    Eredeti karakter tábla
+   001 - O   Ha jobb es van adat: van orajel  value + 256
+   011 - T   "                                value + 768
+   101 - H   "                                value + 1280
+   111 - C   Ha mindharom: van orajel         value + 1792
 
- 0   1   1    Százasok
- 1   0   1    Tizesek
- 1   1   1    Egyesek
- 
+   
 */
 
   Serial.println("Delete bank 0");
   for (int value = 0; value <= 255; value += 1) {
     writeEEPROM(value, 0 );
   }
-/*
+
   Serial.println("Programming ones place");
   for (int value = 0; value <= 255; value += 1) {
-    writeEEPROM(value + 1792, digits[value % 10]);
+    writeEEPROM(value + 256, digits[value % 10]);
   }
   Serial.println("Programming tens place");
   for (int value = 0; value <= 255; value += 1) {
-    writeEEPROM(value + 1280, digits[(value / 10) % 10]);
+    writeEEPROM(value + 768, digits[(value / 10) % 10]);
   }
   Serial.println("Programming hundreds place");
   for (int value = 0; value <= 255; value += 1) {
-    writeEEPROM(value + 768, digits[(value / 100) % 10]);
+    writeEEPROM(value + 1280, digits[(value / 100) % 10]);
   }
-
   Serial.println("Programming Character table");
   int num = 0;
   for (int value = 0; value <= 255; value += 1) {
-    //writeEEPROM(value + 256, num + 32 );
-    writeEEPROM(value + 256, num  );
+    writeEEPROM(value + 1792, num  );
     num++;
   }
-*/
-  Serial.println("Writing init program");
-  
-  writeEEPROM(0, 0x38 );         // Function set, 8 bit, 2 lines, 5x7 
-  writeEEPROM(1, 0xF );          // Display ON, Cursor On, Cursor Blinking 
-  writeEEPROM(2, 0x6 );          // Entry Mode, Increment cursor position, No display shift 
-  writeEEPROM(3, 0x1 );          // Display clear
 
-
-  writeEEPROM(4, 0b01001111 );   // Char: O
-  writeEEPROM(5, 0b01001011 );   // Char: K
-  writeEEPROM(6, 0b00101110 );   // Dot
-  writeEEPROM(7, 0b00101110 );   // Dot
 /*
   Serial.println("Programming commands");
   int number = 0;
@@ -170,67 +157,40 @@ RRW ROE RCS
   }
 */
 
+  Serial.println("Writing init program");
+  /*
+  writeEEPROM(0, 0x38 );         // Function set, 8 bit, 2 lines, 5x7 
+  writeEEPROM(1, 0xF );          // Display ON, Cursor On, Cursor Blinking 
+  writeEEPROM(2, 0x6 );          // Entry Mode, Increment cursor position, No display shift 
+  writeEEPROM(3, 0x1 );          // Display clear
+  */
+/*
+  writeEEPROM(0, 0x30 );   // 8bit, basic instruction set
+  writeEEPROM(1, 0x0c );   // Display ON, Cursor OFF, Blink OFF
+  writeEEPROM(2, 0x01 );   // Display clear
+  writeEEPROM(3, 0x06 );   // Entry mode set
 
-
-// 2  1  3
-
+  writeEEPROM(4, 0b01001111 );   // Char: O
+  writeEEPROM(5, 0b01001011 );   // Char: K
+  writeEEPROM(6, 0x20 );   // 
+  writeEEPROM(7, 0x20 );   // 
+*/
 
 //***********************************************************************************************************************************
 //Modifying delete leading zeros
 //Added Code not to display leading zeros
 
 Serial.println("Modifying tens place");  // Write 0's to 10 addresses starting at address 256 (100h)  9 digits
-  for (int value = 1280; value <= 1289; value += 1) {
+  for (int value = 768; value <= 777; value += 1) {
     writeEEPROM(value, 0x0);
   }
 
 Serial.println("Modifying hundreds place");  // Write 0's to 100 addresses starting at address 512 (200h)  99 digits
-  for (int value = 768; value <= 867; value += 1) {
+  for (int value = 1280; value <= 1379; value += 1) {
     writeEEPROM(value, 0x0);    
   }
 
-  
-/*
-
-Serial.println("Modifying tens place (twos complement)");  // Write 0's to 10 addresses starting at address 1280 (100h)  9 digits     9
-  for (int value = 1280; value <= 1289; value += 1) {
-    writeEEPROM(value, 0xA0);
-  }
-
-Serial.println("Modifying hundreds place (twos complement)");  // Write 0's to 100 addresses starting at address 1536 (200h)  99 digits
-  for (int value = 1536; value <= 1635; value += 1) {
-    writeEEPROM(value, 0xA0);    
-   }  
-
-
-
-
-
-Serial.println("Modifying tens place (twos complement)");  // Write 0's to 10 addresses starting at address 1527 (100h)  9 digits
-  for (int value = 1527; value <= 1535; value += 1) {
-    writeEEPROM(value, 0xA0);
-  }
-
-Serial.println("Modifying hundreds place (twos complement)");  // Write 0's to 100 addresses starting at address 1693 (200h)  99 digits
-  for (int value = 1693; value <= 1791; value += 1) {
-    writeEEPROM(value, 0xA0);
-   }  
-   
-*/
 //***********************************************************************************************************************************
-
-
-
-
-
-
-
-
-
-
-
-
-  
 
 
   // Read and print out the contents of the EERPROM
@@ -238,8 +198,4 @@ Serial.println("Modifying hundreds place (twos complement)");  // Write 0's to 1
   printContents();
 }
 
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
